9149528ea29d15589945acfe3db6b431a2b63cd7
manojdeo <manojdeo@gmail.com>
2020-04-26 10:16:42 +0530

    changes in response

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -10,7 +10,7 @@ public class HelloWorldController {
 
 	@GetMapping(path = "/hello-world")
 	public String helloWorld() {
-		return "Hello World code pipeline";
+		return "Hello World code pipeline1";
 	}
 
 	@GetMapping(path = "/hello-world-bean")
